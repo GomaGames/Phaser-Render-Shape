2d81c2233c5f423e319c460abc92853bdbf70102
Jon Borgonia <jon@gomagames.com>
2016-01-14 16:49:35 -1000

    draw a shape

--- public/js/states/game.js
+++ public/js/states/game.js
@@ -1,6 +1,14 @@
 // class constructor
 RenderShape.Game = function () {
 
+  this.shape;
+  this.shape_props = {
+    FILL_COLOR : 0x6595FF,
+    LINE_COLOR : 0x65FA95,
+    LINE_THICKNESS : 2,
+    SIZE : 100
+  };
+
 };
 
 RenderShape.Game.prototype.preload = function(){
@@ -8,6 +16,13 @@ RenderShape.Game.prototype.preload = function(){
 };
 
 RenderShape.Game.prototype.create = function(){
+
+  this.shape = this.game.add.graphics((this.stage.width - this.shape_props.SIZE) / 2, (this.stage.height - this.shape_props.SIZE) / 2);
+
+  // set a fill and line style
+  this.shape.beginFill(this.shape_props.FILL_COLOR);
+  this.shape.lineStyle(this.shape_props.LINE_THICKNESS, this.shape_props.LINE_COLOR, 1);
+  this.shape.drawRect(0, 0, this.shape_props.SIZE, this.shape_props.SIZE);
 
 };
 
